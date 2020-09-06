7c83022c62636fefa35b85acc643251c11812a6d
Tim Renner <renner.timothy@gmail.com>
2020-09-06 15:41:40 -0500

    Initial commit.

---
+++ scripts/add_column.py
@@ -0,0 +1,13 @@
+import typer
+import pandas as pd
+import numpy as np
+
+
+def main(input_file: str, output_file: str):
+    input_frame = pd.read_csv(input_file)
+    input_frame.loc[:, "extra_column"] = np.random(input_frame.shape[0])
+    input_frame.to_csv(output_file, index=False)
+
+
+if __name__ == "__main__":
+    typer.run(main)
