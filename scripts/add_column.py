4e95172dcf5a80787caba0345ade82724c75842b
Tim Renner <renner.timothy@gmail.com>
2020-09-25 16:18:46 -0500

    Added params.yaml to the sub pipelines. Using OmegaConf to load and handle the file.

--- scripts/add_column.py
+++ scripts/add_column.py
@@ -2,8 +2,15 @@ import typer
 import pandas as pd
 import numpy as np
 
+from omegaconf import OmegaConf
 
-def main(input_file: str, output_file: str):
+conf = OmegaConf.load("params.yaml")
+
+
+def main(
+    input_file: str = conf.preprocess.input_file,
+    output_file: str = conf.preprocess.output_file,
+):
     input_frame = pd.read_csv(input_file)
     input_frame.loc[:, "extra_column"] = np.random.random(input_frame.shape[0])
     input_frame.to_csv(output_file, index=False)
